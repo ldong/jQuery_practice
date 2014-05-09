ff758512e4ec34badaf2d55ca413e6f2ec9583f3
Lin Dong <lindong@gatech.edu>
2014-05-08 23:24:49 -0400

    Add two buttons to change css with bootstrap2, 3

---
+++ js/my_website.js
@@ -0,0 +1,26 @@
+/**
+ * Created by ldong on 5/7/14.
+ */
+
+(function(){
+     var link = $('link').first();
+     $('button').on('click', function(){
+         $this = $(this);
+         console.log($this);
+         $this
+             .siblings('button')
+             .removeAttr('disabled')
+             .end()
+             .attr('disabled', 'disabled');
+
+         var changed_css = $this.data('css');
+         console.log(changed_css);
+         link.attr('href', changed_css);
+
+
+
+//         this_button = $(this).text("Sup Dude");
+//         event.preventDefault();
+//         $(this_button).hide( "slow" );
+     });
+})();
