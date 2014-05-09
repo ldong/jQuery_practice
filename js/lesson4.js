ec7c656a19aef5adbf2e09b7761e5d1442366a6b
Lin Dong <lindong@gatech.edu>
2014-05-09 02:44:37 -0400

    Add appendTo, prependTo insertAfter insertBefore

--- js/lesson4.js
+++ js/lesson4.js
@@ -7,4 +7,26 @@
     $('article').prepend('prepend!');
     $('article').before('before!');
     $('article').after('after!');
+    $('<h2></h2>', {
+        text: 'appendTo article',
+        class: 'myClass'
+    }).appendTo('article');
+
+    $('<h2></h2>', {
+        text: 'prependTo article',
+        class: 'myClass'
+    }).prependTo('article');
+
+    $('<h2></h2>', {
+        text: 'insertAfter article',
+        class: 'myClass'
+    }).insertAfter('article'); // create element then insert after
+
+    $('h2').after('after h2'); // again
+
+    $('<h2></h2>', {
+        text: 'insertBefore article',
+        class: 'myClass'
+    }).insertBefore('article'); // create element then insert before
+
 })();
