92799fc1542a556c2fd2f090d79633e0ad4047de
Lin Dong <lindong@gatech.edu>
2014-05-09 03:15:01 -0400

    Add style for lesson4

--- js/lesson4.js
+++ js/lesson4.js
@@ -3,39 +3,15 @@
  */
 
 (function(){
-//    $('article').append('append!');
-//    $('article').prepend('prepend!');
-//    $('article').before('before!');
-//    $('article').after('after!');
-//    $('<h2></h2>', {
-//        text: 'appendTo article',
-//        class: 'myClass'
-//    }).appendTo('article');
-//
-//    $('<h2></h2>', {
-//        text: 'prependTo article',
-//        class: 'myClass'
-//    }).prependTo('article');
-//
-//    $('<h2></h2>', {
-//        text: 'insertAfter article',
-//        class: 'myClass'
-//    }).insertAfter('article'); // create element then insert after
-//
-//    $('h2').after('after h2'); // again
-//
-//    $('<h2></h2>', {
-//        text: 'insertBefore article',
-//        class: 'myClass'
-//    }).insertBefore('article'); // create element then insert before
+    var co = $('article').find('span.co').each(function(){
+//        $(this).closest('p').prepend('Hi dude');
+        var $this = $(this);
 
-    // using appendTo from existing elem to another, will simply move this elem
-//    $('p').eq(0).after($('h2'));
-//    $('p').first().after($('h2'));
-
-    // swap two elements
-    $('p').eq(0).after(function(){
-        return $(this).prev();
+        $('<blockquote></blockquote>', {
+            class: 'co',
+//            text: $this.text()
+            text: 'Hello JS'
+        }).prependTo($this.closest('p'));
     });
 
 
