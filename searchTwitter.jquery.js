3b7e9a89fa1c57587ace2ea18aea514cf2915ad7
Lin Dong <ldong@synapp.io>
2014-05-15 22:36:26 -0400

    Add configuration for lesson13

--- searchTwitter.jquery.js
+++ searchTwitter.jquery.js
@@ -1,21 +1,33 @@
 // utility
 if(typeof Object.create !== 'function'){
     Object.create = function(obj){
-        function F();
+        function F(){};
         F.prototype = obj;
         return new F();
-    }
+    };
+    
 }
 // plugin pattern
 (function($, window, document, undefined){
     var Twitter = {
-        init: function(){
+        init: function(options, elem){
+            var self = this;
             this.prop ='value';
+            self.elem = elem;
+            self.$elem = $(elem);
+            self.url = 'http://search.twitter.com/search.json';
+            if (typeof options === 'string'){
+                self.search = options;
+            } else {
+                self.search = options.search; 
+                self.options = $.extend({}, $.fn.queryTwitter.options, options);
+            }
         }
     };
     
     $.fn.queryTwitter = function(){
         return this.each(function(){
+
             var twitter = Object.create(Twitter);
             twitter.init();
             
@@ -23,7 +35,7 @@ if(typeof Object.create !== 'function'){
     };
     
     $.fn.queryTwitter.options = {
-        
+        search: '@tutspremium'
     };
     
 })(jQuery, window, document);
